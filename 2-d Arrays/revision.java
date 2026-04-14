de2fe239753a80363bef421b5b46139c615a4f56
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2026-04-14 10:26:21 +0530

    revision added

---
+++ 2-d Arrays/revision.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class revision {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bismillah");
+
+        // the variable names can also start with the symbol $
+
+        System.out.println("Enter the value to check weaather the variabe can take values if there name starts with '$'");
+        int $num = sc.nextInt();
+        System.out.println("The value of $num is : "+ $num);
+        System.out.println("Hence proved as we can use $ symbol for naming the variables ");
+
+    }
+}
